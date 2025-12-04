547441a3a72ff89bb1e4767d8796d13c37f0b493
Ksy0309 <swim040309@gmail.com>
2025-12-04 14:24:48 +0900

    Change in master branch

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -4,5 +4,6 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Hello Git!222222222222");
+		System.out.println("Hello Git!333333333333");
 	}
 }
